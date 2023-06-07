9d8a196e426fbac504a7a20ddcd63e967c9e6c4c
kiranreddy023 <vkirankumar023@gmail.com>
2023-06-07 08:22:00 +0530

    logcompress

--- shellScripting/findpoframmem.sh
+++ shellScripting/findpoframmem.sh
@@ -6,14 +6,14 @@ availMem=$(free -m | awk '/Mem:/ {print $NF}')
 echo "Available RAM Memory is $availMem MB"
 echo "\nTotal RAM Memory is $totalMem MB"
 
-thresold=70
+thresold=30
 echo "\nThresold Limit is $thresold%\n"
 
-present=$((  $availMem *100 / $totalMem ))
+present=$((  $availMem * 100 / $totalMem ))
 
-echo "Used Memory Percantage is $present% \n"
+echo "Available Memory Percantage is $present% \n"
 
-if [ $present -ge $thresold ]
+if [ $thresold -ge $present ]
 then
 	echo "RAM memory usage is exceeded the thresold limit"
 else
