141a420b81c5543ee88d245cfa0254e60404b82c
KIRAN KUMAR V <109744725+kiranreddy023@users.noreply.github.com>
2024-02-28 06:51:55 +0530

    find a file with content

--- shellScripting/findFileWithContent.sh
+++ shellScripting/findFileWithContent.sh
@@ -1,6 +1,26 @@
 #!/bin/bash
-if [ $# -ne 1 ]; then
- echo "usage: $0 <folder/path>"
+
+# check whether folder path is given or not
+if [ $# -ne 2 ]; then
+ echo "usage: $0 <folder/path> <search_word>"
  exit 1
 fi
+
+folder="$1"
+
+
+# check whether given input is a correct path or not
+if [ ! -d "$folder" ]; then
+ echo "Error: '$folder' is not a folder"
+ exit 1
+fi
+
+echo "searching for the word '$2' in '$1' folder"
+
+foundFiles=$(grep -rl '$2' '$folder')
+
+
+
+
+
 
